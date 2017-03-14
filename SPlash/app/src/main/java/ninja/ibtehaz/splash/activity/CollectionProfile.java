6ad0b2d0d4e5efe7e258b6296c3a9a1467118ccc
Asif Ibtehaz <asif.ibtehaz@gagagugu.com>
2017-03-14 13:12:47 +0600

    collection profile created and handled.

---
+++ SPlash/app/src/main/java/ninja/ibtehaz/splash/activity/CollectionProfile.java
@@ -0,0 +1,120 @@
+package ninja.ibtehaz.splash.activity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import ninja.ibtehaz.splash.R;
+import ninja.ibtehaz.splash.models.CollectionModel;
+import ninja.ibtehaz.splash.utility.Util;
+
+/**
+ * Created by ibtehaz on 3/14/17.
+ */
+
+public class CollectionProfile extends BaseActivity implements View.OnClickListener {
+
+    private Context context;
+    private CollectionModel collectionModel;
+    private String collectionId;
+    private Toolbar toolbar;
+
+    private TextView txtTitle, txtName, txtDescription, txtTotalPhotos, txtUpdated, txtPublished;
+    private ImageView imgCover, imgProfile, imgLayer;
+    private Util util;
+    private ImageView imgBack;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_profile);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*------------------------------------------------------*/
+        init();
+        /*------------------------------------------------------*/
+        getExtra();
+        /*------------------------------------------------------*/
+        inflateUi();
+        /*------------------------------------------------------*/
+
+    }
+
+
+    /**
+     *
+     */
+    private void getExtra() {
+        this.collectionModel = (CollectionModel)getIntent().getSerializableExtra("collection_data");
+        this.collectionId = collectionModel.getCollectionId();
+    }
+
+
+    /**
+     *
+     */
+    private void init() {
+        this.context = this;
+        this.util = new Util();
+
+        txtTitle = (TextView)findViewById(R.id.txt_title);
+        txtName = (TextView)findViewById(R.id.txt_user_name);
+        txtDescription = (TextView)findViewById(R.id.txt_description);
+        txtTotalPhotos = (TextView)findViewById(R.id.txt_total_photos);
+        txtUpdated = (TextView)findViewById(R.id.txt_last_updated);
+        txtPublished = (TextView)findViewById(R.id.txt_published);
+
+        imgCover = (ImageView)findViewById(R.id.img_cover);
+        imgLayer = (ImageView)findViewById(R.id.img_layer);
+        imgProfile = (ImageView)findViewById(R.id.img_profile);
+        imgBack = (ImageView)findViewById(R.id.img_back);
+
+        imgBack.setOnClickListener(this);
+    }
+
+
+    /**
+     *
+     */
+    private void inflateUi() {
+        txtTitle.setText(util.capitalizeWords(collectionModel.getCollectionTitle()) + " by: "+
+                util.capitalizeWords(collectionModel.getUsername()));
+
+        if (!collectionModel.getCollectionDescription().equalsIgnoreCase("none")) {
+            txtDescription.setText(collectionModel.getCollectionDescription());
+            txtTotalPhotos.setTextColor(ContextCompat.getColor(context, R.color.secondaryColor));
+        } else {
+            txtDescription.setVisibility(View.GONE);
+            txtTotalPhotos.setTextColor(ContextCompat.getColor(context, R.color.primaryTextColor));
+        }
+        txtTotalPhotos.setText(collectionModel.getTotalPhotos() + " Photos");
+
+        imgLayer.setBackgroundColor(Color.parseColor(collectionModel.getCoverColor()));
+        util.loadImage(context, collectionModel.getUrlRegular(), imgCover, imgLayer);
+        util.loadProfilePic(context, collectionModel.getProfileImageLarge(), imgProfile);
+        txtPublished.setText(collectionModel.getPublishedAt());
+        txtUpdated.setText(collectionModel.getUpdatedAt());
+
+        util.makeToast(context, collectionId + " collection id");
+    }
+
+    /**
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.img_back:
+                finish();
+                break;
+        }
+    }
+}
--- SPlash/app/src/main/java/ninja/ibtehaz/splash/viewHolder/CollectionViewHolder.java
+++ SPlash/app/src/main/java/ninja/ibtehaz/splash/viewHolder/CollectionViewHolder.java
@@ -1,13 +1,16 @@
 package ninja.ibtehaz.splash.viewHolder;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import ninja.ibtehaz.splash.R;
+import ninja.ibtehaz.splash.activity.CollectionProfile;
 import ninja.ibtehaz.splash.models.CollectionModel;
 import ninja.ibtehaz.splash.utility.Util;
 
@@ -15,12 +18,14 @@ import ninja.ibtehaz.splash.utility.Util;
  * Created by ibtehaz on 3/13/17.
  */
 
-public class CollectionViewHolder extends RecyclerView.ViewHolder {
+public class CollectionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     private Context context;
     private ImageView imgOriginal, imgLayer;
     private TextView txtCollectionTitle, txtUserName;
     private Util util;
+    private CollectionModel currentModel;
+    private LinearLayout llCollection;
 
 
     /**
@@ -37,7 +42,9 @@ public class CollectionViewHolder extends RecyclerView.ViewHolder {
         imgLayer = (ImageView)itemView.findViewById(R.id.img_layer);
         txtCollectionTitle = (TextView)itemView.findViewById(R.id.txt_title);
         txtUserName = (TextView)itemView.findViewById(R.id.txt_author);
+        llCollection = (LinearLayout)itemView.findViewById(R.id.ll_collection);
 
+        llCollection.setOnClickListener(this);
     }
 
 
@@ -46,9 +53,33 @@ public class CollectionViewHolder extends RecyclerView.ViewHolder {
      * @param model
      */
     public void onBindData(CollectionModel model) {
+        this.currentModel = model;
         imgLayer.setBackgroundColor(Color.parseColor(model.getCoverColor()));
         util.loadImage(context, model.getUrlRegular(), imgOriginal, imgLayer);
         txtCollectionTitle.setText(util.capitalizeWords(model.getCollectionTitle()));
         txtUserName.setText(util.capitalizeWords(model.getUsername()));
+    }
+
+    /**
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_collection:
+                openCollectionProfile();
+                break;
+        }
+    }
+
+
+    /**
+     *
+     */
+    private void openCollectionProfile() {
+        Intent i = new Intent(context, CollectionProfile.class);
+        i.putExtra("collection_data", currentModel);
+        context.startActivity(i);
     }
 }
