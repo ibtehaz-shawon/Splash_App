d21bdb054dbf5d822f1593167b79903ab9ac4ec5
Asif Ibtehaz <asif.ibtehaz@gagagugu.com>
2017-05-22 15:51:42 +0600

    some codes added for alert dialog - on click listener

--- app/src/main/java/ninja/ibtehaz/splash/viewHolder/FeedViewHolder.java
+++ app/src/main/java/ninja/ibtehaz/splash/viewHolder/FeedViewHolder.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.IdRes;
 import android.support.v7.widget.DialogTitle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,6 +13,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import ninja.ibtehaz.splash.R;
@@ -124,8 +127,49 @@ public class FeedViewHolder extends RecyclerView.ViewHolder implements View.OnCl
         final Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.item_daily_wallpaper_confirm);
 
+        RadioGroup rGroupChangeTime = (RadioGroup)dialog.findViewById(R.id.rGroup_wallpaper_change);
+        RadioGroup rGroupWallpaperNumber = (RadioGroup)dialog.findViewById(R.id.rGroup_wallpaper_number);
         final CheckBox chkIsOffline = (CheckBox)dialog.findViewById(R.id.cBox_offline_mode);
         Button btnConfirm = (Button)dialog.findViewById(R.id.btn_confirm);
+
+        /**
+         * on click listener to handle and detect time listener (When to change wallpaper)
+         */
+        rGroupChangeTime.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (group.getCheckedRadioButtonId()) {
+                    case R.id.rButton_six_am:
+                        //6 am
+                        break;
+                    case R.id.rButton_twelve_am:
+                        //12 am
+                        break;
+                }
+            }
+        });
+
+
+        /**
+         * on click listener to check and hold number of wallpapers to be downloaded at a time.
+         */
+        rGroupWallpaperNumber.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (group.getCheckedRadioButtonId()) {
+                    case R.id.rBtn_five:
+                        break;
+
+                    case R.id.rBtn_ten:
+                        break;
+
+                    case R.id.rBtn_fifteen:
+                        break;
+                }
+            }
+        });
+
+
 
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,6 +177,7 @@ public class FeedViewHolder extends RecyclerView.ViewHolder implements View.OnCl
                 boolean isOffline = chkIsOffline.isChecked();
                 dailyWallpaper.onDailyPaperSet(isOffline);
                 if (isOffline) {
+                    //do some shit here
                     util.makeToast(context, "We are downloading the wallpapers. It will take a while!");
                 } else {
                     util.makeToast(context, "Wallpapers are being set! Please check Settings for changes!");
